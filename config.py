696d040398f37fbe167fb76ad03f8f88f901d81b
Aꪝ 0x┋TEAM ✇ <newbashrabiu@gmail.com>
2025-09-25 16:55:25 +0100

    Update and rename config (1).py to config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+BOT_TOKEN = "Your Bot Token"
+
+CHANNEL_LINK = "Your Channel link"
+
+CHANNEL_ID = -Your channel id # Ka tabbata wannan lambar daidai
+
+OTP_GROUP_LINK = "Your Otp group"
+
+ADMINS = [Your admin id]
+
+DB_PATH = "database.db"
